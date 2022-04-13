aa834dcaf9129d4beab1e490bf4dd2dd9fd17be5
Masaru Ohashi Jr <masaru.junior@previc.gov.br>
2022-04-12 21:19:53 -0300

    first commit

--- database/dbNumber.go
+++ database/dbNumber.go
@@ -25,22 +25,40 @@ func createTableNumber(db *sql.DB) {
 
 func insertNumbers() {
 	insertNumber(Db, 1, "+1 204-800-3082", "DI777c3e320f2f657cfb9d418ca591a6a6", "")
+	insertPhoneNumber(Db, 1, 1, 1, "NumberA", 1)
 	insertNumber(Db, 2, "+1 732-401-9498", "DI777c3e3217428380ead64b358f23a6ce", "")
+	insertPhoneNumber(Db, 2, 1, 2, "NumberB", 2)
 	insertNumber(Db, 3, "+1 204-800-3047", "DI777c3e32697ba80d5a4f4082bd4dcff5", "")
+	insertPhoneNumber(Db, 3, 1, 3, "NumberC", 3)
 	insertNumber(Db, 4, "+1 204-800-3029", "DI777c3e3270908f97680344b5a3fea522", "")
+	insertPhoneNumber(Db, 4, 1, 4, "NumberD", 4)
 	insertNumber(Db, 5, "+1 204-800-3030", "DI777c3e32d3e6623dbbe740ea93fdebb4", "")
+	insertPhoneNumber(Db, 5, 1, 5, "NumberE", 5)
 }
 
 // We are passing db reference connection from main to our method with other parameters
 func insertNumber(db *sql.DB, id int, phoneNumber, sid string, description string) {
 	log.Println("Inserting number record ...")
 	insertNumberSQL := `INSERT INTO number(id, phoneNumber, sid, description) VALUES (?, ?, ?, ?)`
+
 	statement, err := db.Prepare(insertNumberSQL) // Prepare statement.
 	// This is good to avoid SQL injections
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
 	_, err = statement.Exec(id, phoneNumber, sid, description)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func insertPhoneNumber(db *sql.DB, id, contextId, phoneNumberId int, alias string, position int) {
+	insertNumberContextSQL := "INSERT INTO phoneNumberContext(id, contextId, phoneNumberId, alias, position) VALUES (?,?,?,?,?)"
+	statement, err := db.Prepare(insertNumberContextSQL)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(id, contextId, phoneNumberId, alias, position)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
--- database/dbParameter.go
+++ database/dbParameter.go
@@ -23,14 +23,20 @@ func createTableParameter(db *sql.DB) {
 
 func insertParameters() {
 	insertParameter(Db, 1, "ApiUrl", "")
+	insertParameterContext(Db, 1, 1, 1, "https://api.zang.io", 1)
 	insertParameter(Db, 2, "BaseUrl", "")
+	insertParameterContext(Db, 2, 1, 2, "http://zarbat.ngrok.io", 2)
 	insertParameter(Db, 3, "ApiVersion", "")
+	insertParameterContext(Db, 3, 1, 3, "v2", 3)
 	insertParameter(Db, 4, "AccountSid", "")
+	insertParameterContext(Db, 4, 1, 4, "AC777c3e3262352901b0e24b7092f6eef0", 4)
 	insertParameter(Db, 5, "AuthToken", "")
+	insertParameterContext(Db, 5, 1, 5, "c59b774e18b44769bdc8b4253a7ee0d7", 5)
 	insertParameter(Db, 6, "From", "")
 	insertParameter(Db, 7, "To", "")
 	insertParameter(Db, 8, "ToSid", "")
 	insertParameter(Db, 9, "ActionUrl", "")
+	insertParameterContext(Db, 6, 1, 6, "http://zang.io/ivr/welcome/call", 6)
 	insertParameter(Db, 10, "StatusCallback", "")
 }
 
@@ -44,6 +50,20 @@ func insertParameter(db *sql.DB, id int, name, description string) {
 		log.Fatalln(err.Error())
 	}
 	_, err = statement.Exec(id, name, description)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func insertParameterContext(db *sql.DB, id, contextId, parameterId int, value string, position int) {
+	log.Println("Inserting parameter record ...")
+	insertParameterSQL := `INSERT INTO parameterContext(id, contextId, parameterId, value, position) VALUES (?, ?, ?, ?, ?)`
+	statement, err := db.Prepare(insertParameterSQL) // Prepare statement.
+	// This is good to avoid SQL injections
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(id, contextId, parameterId, value, position)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
